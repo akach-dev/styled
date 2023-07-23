2b3db7e4c032b501fc0ade32a05bf15243761801
Alexander Kachan <akach.dev@mail.ru>
2023-07-23 16:30:28 +0300

    add global styles

---
+++ src/styles/Global.styled.tsx
@@ -0,0 +1,71 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+  body {
+    font-family: 'Poppins', sans-serif, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  /* Обнуляющий стиль */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+  }
+
+  a,
+  a:visited {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  button {
+    background-color: unset;
+    border: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: inherit;
+    font-size: inherit;
+  }
+
+  img {
+    vertical-align: top;
+  }
+
+  body,
+  html {
+    height: 100%;
+    line-height: 1;
+  }
+
+  main {
+    flex: 1 1 auto;
+    background-color: antiquewhite;
+  }
+
+  /* ================================================================ */
+
+`
