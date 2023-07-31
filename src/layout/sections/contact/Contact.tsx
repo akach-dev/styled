509a3cb48e54a3192be9bee494df434a22086878
Alexander Kachan <akach.dev@mail.ru>
2023-07-31 21:18:38 +0300

    refactoring Contact component done

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -1,62 +1,23 @@
 import React from 'react';
-import styled from "styled-components";
 import {Title} from "../../../components/Title";
 import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
-import {theme} from "../../../styles/Theme";
+import {S} from './Contact_Styles';
 
 export const Contact = () => {
   return (
-     <StyledContact>
+     <S.Contact>
        <Container>
          <Title>Contact</Title>
-         <StyledForm>
-           <Field placeholder={'Name'}/>
-           <Field placeholder={'second Name'}/>
-           <Field placeholder={'message'} as={'textarea'}/>
+         <S.Form>
+           <S.Field placeholder={'Name'}/>
+           <S.Field placeholder={'second Name'}/>
+           <S.Field placeholder={'message'} as={'textarea'}/>
            <Button>Send message</Button>
-         </StyledForm>
+         </S.Form>
        </Container>
-     </StyledContact>
+     </S.Contact>
   );
 };
 
 
-const StyledContact = styled.section`
-  h2:not(:last-child) {
-    margin-bottom: 2.25em;
-  }
-`
-const StyledForm = styled.form`
-  max-width: 500px;
-  width: 100%;
-  margin: 0 auto;
-
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  textarea {
-    resize: none;
-    height: 150px;
-  }
-`
-const Field = styled.input`
-  border: 1px solid ${theme.colors.borderColor};
-  color: ${theme.colors.font};
-  background: ${theme.colors.secondaryBg};
-  width: 100%;
-  padding: 7px 15px;
-  font-size: 0.75rem;
-  font-family: Poppins, sans-serif;
-
-  &::placeholder {
-    color: ${theme.colors.placeholderColor};
-    text-transform: capitalize;
-  }
-
-  &:focus-visible {
-    outline: 1px solid ${theme.colors.borderColor};
-  }
-
-`
---
+++ src/layout/sections/contact/Contact_Styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme";
+
+const Contact = styled.section`
+  h2:not(:last-child) {
+    margin-bottom: 2.25em;
+  }
+`
+const Form = styled.form`
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  textarea {
+    resize: none;
+    height: 150px;
+  }
+`
+const Field = styled.input`
+  border: 1px solid ${theme.colors.borderColor};
+  color: ${theme.colors.font};
+  background: ${theme.colors.secondaryBg};
+  width: 100%;
+  padding: 7px 15px;
+  font-size: 0.75rem;
+  font-family: Poppins, sans-serif;
+
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+    text-transform: capitalize;
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
+`
+export const S = {
+  Contact, Form, Field,
+}
