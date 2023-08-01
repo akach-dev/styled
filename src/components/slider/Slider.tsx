547443460804cbaf3d27eb26904306fe8f66fc59
Alexander Kachan <akach.dev@mail.ru>
2023-08-01 21:22:18 +0300

    slider React Alice Carousel done

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,23 +1,49 @@
 import React, {FC} from 'react';
-import {FlexWrapper} from "../FlexWrapper";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import {S} from './Slider_Styles';
+import '../../styles/slider.css'
 
-export const Slider: FC = () => {
+type SlidePropsTypes = {
+  name: string
+  text: string
+}
+
+const Slide = ({name, text}: SlidePropsTypes) => {
   return (
-     <S.Slider>
-       <FlexWrapper>
-         <S.Slide>
-           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-             dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-           <S.Name>@ivan ivanow</S.Name>
-         </S.Slide>
-       </FlexWrapper>S.
-       <S.Pagination>
-         <span> </span>
-         <span className={'active'}> </span>
-         <span> </span>
-       </S.Pagination>
-     </S.Slider>
-  );
-};
+     <S.Slide>
+       <p>{text}</p>
+       <S.Name>@{name}</S.Name>
+     </S.Slide>
+  )
+}
+
+const items = [
+  <Slide
+     text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid amet cupiditate delectus est molestiae nostrum provident tenetur veritatis? Aliquam dicta earum laboriosam maiores pariatur perspiciatis quos recusandae repudiandae.'}
+     name={'ivan ivanow'}/>,
+  <Slide
+     text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequatur cumque ea magnam nobis perspiciatis possimus, rerum voluptatibus. Adipisci alias delectus eum impedit nihil optio praesentium sed sit veniam voluptas?'}
+     name={'alex kach'}/>,
+  <Slide
+     text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta nostrum nulla numquam quibusdam, reprehenderit sunt tempora tenetur voluptatem voluptatibus voluptatum. Alias dolorem dolores enim excepturi harum hic laboriosam non, quia.'}
+     name={'max kach'}/>,
+];
+
+export const Slider: FC = () => (
+
+   <S.Slider>
+     <AliceCarousel
+        animationType="fadeout"
+        animationDuration={800}
+        disableButtonsControls
+        infinite
+        items={items}
+        mouseTracking
+     />
+   </S.Slider>
+
+
+);
+
 
--- src/components/slider/Slider_Styles.ts
+++ src/components/slider/Slider_Styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import {theme} from "../../styles/Theme";
 
 const Slider = styled.div`
   max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   & > :not(:last-child) {
     margin-bottom: 1.25em;
@@ -27,25 +30,6 @@ const Name = styled.span`
   text-transform: uppercase;
 `
 
-const Pagination = styled.div`
-  display: inline-flex;
-  gap: 5px;
-
-  span {
-    border-radius: 50%;
-    height: 7px;
-    width: 7px;
-    background-color: rgba(255, 255, 255, 0.5);
-    cursor: pointer;
-  }
-
-  .active {
-    width: 20px;
-    border-radius: 35px;
-    background-color: ${theme.colors.accent};
-  }
-`
-
 export const S = {
-  Name, Slider, Slide, Pagination
+  Name, Slider, Slide
 }
