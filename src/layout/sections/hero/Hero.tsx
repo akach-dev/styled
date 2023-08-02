771f940e5d6b4c158b6b2a985bde259cff8ffe45
Alexander Kachan <akach.dev@mail.ru>
2023-08-02 17:29:57 +0300

    React-parallax-tilt done

--- src/layout/sections/hero/Hero.tsx
+++ src/layout/sections/hero/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import hero from "../../../assets/images/photo.webp"
 import {S} from './Hero_Styles';
 import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Hero = () => {
   return (
@@ -27,9 +28,21 @@ export const Hero = () => {
            </S.HeroSubTitle>
          </S.HeroBody>
 
-         <S.HeroImg>
-           <S.Img src={hero} alt="my img"/>
-         </S.HeroImg>
+
+         <Tilt
+            className="parallax-effect-img"
+            tiltMaxAngleX={40}
+            tiltMaxAngleY={40}
+            perspective={800}
+            transitionSpeed={1500}
+            scale={1.1}
+            gyroscope={true}
+         >
+           <S.HeroImg>
+             <S.Img src={hero} alt="my img"/>
+           </S.HeroImg>
+         </Tilt>
+
 
        </S.HeroContainer>
      </S.Hero>
