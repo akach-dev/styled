355b7a4ea19e68254b6ecb1560145207ea6c6df2
Alexander Kachan <akach.dev@mail.ru>
2023-08-03 18:32:12 +0300

    add  scrollToTop  Logo component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import {Icon} from "../icon/Icon";
+import {animateScroll as scroll} from "react-scroll";
+
 
 export const Logo: FC = () => {
   return (
-     <a href="">
+     <a onClick={() => scroll.scrollToTop()}>
        <Icon iconId={'code'}/>
      </a>
   );
