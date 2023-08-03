4d775016af9e8c2472c03a94207123958cbce5a8
Alexander Kachan <akach.dev@mail.ru>
2023-08-03 19:32:03 +0300

    goTopBtn done

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {Icon} from "../icon/Icon";
 import {animateScroll as scroll} from "react-scroll/modules";
@@ -7,15 +7,27 @@ export const GoTopBtn = () => {
 
   const [showBtn, setShowBtn] = useState(false)
 
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 200 ? setShowBtn(true) : setShowBtn(false)
+    })
+  }, []);
+
   return (
-     <StyledGoTopBtn onClick={() => scroll.scrollToTop()}>
-       <Icon
-          width={'16'}
-          height={'15'}
-          viewBox={'0 0 16 15'}
-          iconId={'arrowGoTop'}/>
-     </StyledGoTopBtn>
-  );
+     <>
+       {
+          showBtn && (
+             <StyledGoTopBtn onClick={() => scroll.scrollToTop()}>
+               <Icon
+                  width={'16'}
+                  height={'15'}
+                  viewBox={'0 0 16 15'}
+                  iconId={'arrowGoTop'}/>
+             </StyledGoTopBtn>
+          )
+       }
+     </>
+  )
 };
 const StyledGoTopBtn = styled.button`
   background-color: rgba(0, 0, 0, 0.3);
