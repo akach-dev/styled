24db5623a60f45ebda9d6222025a0fc280d36a18
Alexander Kachan <akach.dev@mail.ru>
2023-08-07 19:37:19 +0300

    generateAdaptiveStyles  Slogan section done

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import styled from "styled-components";
 import {Title} from "../../../components/Title";
 import {Button} from "../../../components/Button";
+import {generateAdaptiveStyles} from "../../../assets/generateAdaptiveStyles";
 
 export const Slogan: FC = () => {
   return (
@@ -14,5 +15,12 @@ export const Slogan: FC = () => {
   );
 };
 const StyledSlogan = styled.section`
+  ${generateAdaptiveStyles({
+    property: 'padding-bottom',
+    startSize: 100,
+    minSize: 90,
+    widthFrom: 1170,
+    keepSize: 1,
+  })}
   text-align: center;
 `
